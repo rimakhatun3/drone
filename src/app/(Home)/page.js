8f6d8efc3c6bca9c7fa95dcbc84614bbff04c61a
rimakhatun3 <rimamnt46@gmail.com>
2023-08-30 13:55:50 +0600

    update change

--- src/app/(Home)/page.js
+++ src/app/(Home)/page.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import Banner from "./Banner";
-import Image from 'next/image';
-import img1 from "../../../public/drone1.png"
-import img2 from "../../../public/drone2.png"
-import img3 from "../../../public/drone3.png"
-import img4 from "../../../public/drone4.png"
-import img5 from "../../../public/drone5.jpg"
-import img6 from "../../../public/drone6.jpg"
-import img7 from "../../../public/drone7.jpg"
-import img8 from "../../../public/drone8.jpg"
-import img9 from "../../../public/drone8.jpg"
+
 import Photography from './photography';
 import Bestdrone from './Bestdrone';
 import OurClient from './OurClient';
@@ -22,27 +13,7 @@ const page = () => {
             <Banner/>
          
 
-{/* <div className="marquee flex gap-5  border border-spacing-2 py-7 px-10 mx-12 w-[1300px] my-10">
-<Image alt='' width={60} height={20} src={img1}/>
-  <Image alt='' width={60} height={20} src={img2}/>
-  <Image alt='' width={60} height={20} src={img3}/>
-  <Image alt='' width={60} height={20} src={img4}/>
-<Image alt='' width={60} height={20} src={img5}/>
-  <Image alt='' width={60} height={20} src={img6}/>
-  <Image alt='' width={60} height={20} src={img7}/>
-  <Image alt='' width={60} height={20} src={img8}/>
-<Image alt='' width={60} height={20} src={img9}/>
-<Image alt='' width={60} height={20} src={img1}/>
-  <Image alt='' width={60} height={20} src={img2}/>
-  <Image alt='' width={60} height={20} src={img3}/>
-  <Image alt='' width={60} height={20} src={img4}/>
-<Image alt='' width={60} height={20} src={img5}/>
-  <Image alt='' width={60} height={20} src={img6}/>
-  <Image alt='' width={60} height={20} src={img7}/>
 
- 
-  
-    </div> */}
 
     <Photography/>
     <Bestdrone/>
