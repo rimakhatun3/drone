0efcfc6d679a8655f11cbf12331e69cf33fe2082
rimakhatun3 <rimamnt46@gmail.com>
2023-08-30 18:21:44 +0600

    added all

---
+++ src/app/page.jsx
@@ -0,0 +1,27 @@
+import Banner from '@/components/Banner';
+import Bestdrone from '@/components/Bestdrone';
+import Features from '@/components/Features';
+import OurClient from '@/components/OurClient';
+import Photography from '@/components/photography';
+import React from 'react';
+
+
+
+const page = () => {
+    return (
+        <div>
+            <Banner/>
+         
+
+
+
+    <Photography/>
+    <Bestdrone/>
+    <OurClient/>
+    <Features/>
+
+        </div>
+    );
+};
+
+export default page;
--- src/components/Bestdrone.jsx
+++ src/components/Bestdrone.jsx
@@ -1,10 +1,10 @@
 import droneData from '@/data/droneData';
 import React from 'react';
-import SingleDrone from './SingleDrone';
+import SingleDrone from '../app/(Home)/SingleDrone';
 
 const Bestdrone = async () => {
-    const data = await fetch('../../data/droneData.json')
-    const droneData = await res.json(data)
+    // const data = await fetch('../../data/droneData.json')
+    // const droneData = await res.json(data)
     return (
         <div>
             <h1 className='text-5xl font-extrabold text-center mt-10 mb-20'>Our Exclusive Collection</h1>
