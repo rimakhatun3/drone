12dc38eebcc5de5dd61d8eba961d7b07c2199e80
rimakhatun3 <rimamnt46@gmail.com>
2023-09-11 11:12:40 +0600

    update logo

--- src/components/Bestdrone.jsx
+++ src/components/Bestdrone.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 import SingleDrone from './SingleDrone';
 
 const Bestdrone = async () => {
-    // const data = await fetch('../../data/droneData.json')
-    // const droneData = await res.json(data)
+   
     return (
         <div>
             <h1 className='text-5xl font-extrabold text-center mt-10 mb-20'>Our Exclusive Collection</h1>
